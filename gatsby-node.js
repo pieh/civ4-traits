fc080ee275badb3eaf73246da9fa721d71fa2c0f
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-01-27 21:47:29 +0100

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,6 +8,8 @@ exports.sourceNodes = async ({
   const { createNode } = actions;
   const leaders = await scrapeData();
 
+  require(`fs-extra`).writeJSONSync("./leaders.json", leaders);
+
   leaders.forEach(leader => {
     const leaderNode = {
       id: createNodeId(leader.leader),
